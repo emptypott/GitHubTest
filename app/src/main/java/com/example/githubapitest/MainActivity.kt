a7179c2c2637ee595f281b7586efae23e7b5968f
Jeong su Choi <>
2021-07-19 11:29:24 +0900

    adapter binding

--- app/src/main/java/com/example/githubapitest/MainActivity.kt
+++ app/src/main/java/com/example/githubapitest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ablyproject.presentation.adapter.MainAdapter
 import com.example.githubapitest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +13,14 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
+    private lateinit var mainAdapter : MainAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        mainAdapter = MainAdapter()
+        binding.rvHomeGoods.adapter = mainAdapter
 
     }
 }
