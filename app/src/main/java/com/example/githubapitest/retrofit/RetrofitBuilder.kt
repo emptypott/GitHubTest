62ee3f1030b2ea6c9e6d5aa5c301acd72897ebf3
Jeong su Choi <>
2021-07-19 11:26:36 +0900

    retrofitbuilder 완성

--- app/src/main/java/com/example/githubapitest/retrofit/RetrofitBuilder.kt
+++ app/src/main/java/com/example/githubapitest/retrofit/RetrofitBuilder.kt
@@ -2,12 +2,18 @@ package com.example.githubapitest.retrofit
 
 import android.security.identity.AccessControlProfileId
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
+
     var api: API
 
     init {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com/users/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        api = retrofit.create(API::class.java)
     }
 }
